165dd3e2d68e548842d8714ef35a6c3d8d4ad620
Yev Ro <yev.rokhin@gmail.com>
2020-04-26 15:14:31 -0700

    fixed the button functionality - buttons work now.  styled the css a little. added the jQuery source to html just incase I decided to utilize it.

--- script.js
+++ script.js
@@ -1,67 +1,81 @@
-
 var startPage = document.getElementById("start-page");
 var startButton = document.getElementById("startbtn");
-let correctBtn = document.getElementById("correct");
-let incorrectBtn = document.getElementById("incorrect");
 var timer;
 var time = 60;
 var timerEl = document.querySelector(".timer");
-var qu
-
-//EVENT LISTENERS
-startButton.addEventListener("click", startQuiz);
-// correctBtn.addEventListener("click", correctAnswer);
-// incorrectBtn.addEventListener("click", incorrectAnswer);
+//var qu
 
 //QUESTION VARIABLES
-var questionsContainer = document.getElementById("questions")
+var questionsContainer = document.getElementById("questions");
 var questions = questionsContainer.children;
 var questionIndex = 0;
-console.log(questionsContainer)
+
 function showCurrentQuestion() {
+    questionIndex;
     if (questionIndex > 0) {
         questions[questionIndex - 1].classList.add("hide");
     }
     questions[questionIndex].classList.remove("hide");
+};
 
-}
-
-// let lastQuestionIndex = questionsArr.length - 1;
-// let currentQuestionIndex = 0;
-// let questionsArr = questions;
-
-function nextQuestion() {
-    questionIndex + 1;
-}
-
-//START TIMER
+//START TIMER AND RUN OUT OF TIME FUNCTION!
 function startTimer() {
     time--;
     timerEl.textContent = "Time left: " + time;
-
     if (time === 0) {
         clearInterval(timer);
         timerEl.textContent = "YOU ARE OUT OF TIME!";
         questions[questionIndex].classList.add("hide");
     }
 }
-//START QUIZ
+
+//START QUIZ FUNCTION
+startButton.addEventListener("click", startQuiz);
 function startQuiz() {
     timer = setInterval(startTimer, 1000)
     startPage.classList.add("hide");
     showCurrentQuestion();
-
-    // question1.classList.remove("hide");
 }
 
 //CORRECT ANSWER FUNCTION
+var correctBtnContainer = document.getElementsByClassName("correct");
+var correctBtns = correctBtnContainer.children;
+var btnIndx = 0;
+let correctBtn = document.getElementsByClassName("correct")
+
+correctBtn[0].addEventListener("click", correctAnswer)
+correctBtn[1].addEventListener("click", correctAnswer)
+correctBtn[2].addEventListener("click", correctAnswer)
+correctBtn[3].addEventListener("click", correctAnswer)
+
 function correctAnswer() {
     alert("You Got it RIGHT!");
-    ;
-};
-
-//INCORRECT ANSWER FUNCTION
-function incorrectAnswer() {
-    alert("Wow...did you even study bro!?");
+    questionIndex++;
     showCurrentQuestion();
 }
+
+//INCORRECT ANSWER FUNCTION
+var incorrectBtnContainer = document.getElementsByClassName("incorrect");
+var incorrectBtns = correctBtnContainer.children;
+var incbtnIndx = 0;
+let incorrectBtn = document.getElementsByClassName("incorrect")
+
+incorrectBtn[0].addEventListener("click", incorrectAnswer)
+incorrectBtn[1].addEventListener("click", incorrectAnswer)
+incorrectBtn[2].addEventListener("click", incorrectAnswer)
+incorrectBtn[3].addEventListener("click", incorrectAnswer)
+incorrectBtn[4].addEventListener("click", incorrectAnswer)
+incorrectBtn[5].addEventListener("click", incorrectAnswer)
+incorrectBtn[6].addEventListener("click", incorrectAnswer)
+incorrectBtn[7].addEventListener("click", incorrectAnswer)
+incorrectBtn[8].addEventListener("click", incorrectAnswer)
+incorrectBtn[9].addEventListener("click", incorrectAnswer)
+incorrectBtn[10].addEventListener("click", incorrectAnswer)
+incorrectBtn[11].addEventListener("click", incorrectAnswer)
+
+function incorrectAnswer() {
+    alert("Did you even STUDY BRO!?");
+    questionIndex++;
+    showCurrentQuestion();
+};
+
